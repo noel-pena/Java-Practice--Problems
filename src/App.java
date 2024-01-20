f4c012a50ac9ffc41f0bb3bb3b64bab5714f2a93
noel-pena <azulu88@hotmail.com>
2024-01-19 19:04:47 -0500

    app

--- src/App.java
+++ src/App.java
@@ -1,10 +1,53 @@
+import java.util.Arrays;
 import java.util.Date;
 
+////write date
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         //this is how to print out the current time
+//         byte age = 29;
+//         Date now = new Date();
+//         System.out.println(now);
+//     }
+// }
+
+////arrays
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         // int [] numbers = new int[5]; this is a method to write arrays individually 
+//         // numbers[0] = 1;
+//         // numbers[1] = 2;
+//         int [] numbers = { 2, 3, 4, 1}; //this is how you can write an array as a whole
+//         Arrays.sort(numbers);
+//         System.out.println(Arrays.toString(numbers)); //remember toString to output in console
+//     }
+// }
+
+////multidimensional arrays
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         // int [][] numbers = new int[2][3]; //two rows, three columns
+//         // numbers[0][0] = 1;
+//         int [][] numbers = {{1,2,3},{4,5,6}}; //this is the same way to write it but in brackets
+//         System.out.println(Arrays.deepToString(numbers));
+//     }
+// }
+
+////constants
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         final float PI = 3.14F; //use "final" to write a constant
+//     }
+// }
+
+////converting division to decimals
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         double result = (double)10 / (double)3; //this is how to divide and include double to result in a decimal
+//         System.out.println(result);
+//     }
+// }
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        //this is how to print out the current time
-        byte age = 29;
-        Date now = new Date();
-        System.out.println(now);
-    }
+
 }
